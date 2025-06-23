3a0d89624ba8c99ed3d86d1034a487ed41d61db7
iampersistent <corvus@zestic.com>
2025-06-23 18:31:21 -0500

    updates for routes, add health check and ping

--- config/routes.php
+++ config/routes.php
@@ -10,36 +10,15 @@ use Psr\Container\ContainerInterface;
  * FastRoute route configuration
  *
  * @see https://github.com/nikic/FastRoute
- *
- * Setup routes with a single request method:
- *
- * $app->get('/', App\Handler\HomePageHandler::class, 'home');
- * $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
- * $app->put('/album/{id:\d+}', App\Handler\AlbumUpdateHandler::class, 'album.put');
- * $app->patch('/album/{id:\d+}', App\Handler\AlbumUpdateHandler::class, 'album.patch');
- * $app->delete('/album/{id:\d+}', App\Handler\AlbumDeleteHandler::class, 'album.delete');
- *
- * Or with multiple request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
- *
- * Or handling all request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class)->setName('contact');
- *
- * or:
- *
- * $app->route(
- *     '/contact',
- *     App\Handler\ContactHandler::class,
- *     Mezzio\Router\Route::HTTP_METHOD_ANY,
- *     'contact'
- * );
  */
 
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
-    $app->get('/', App\Handler\HomePageHandler::class, 'home');
-    $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
+    $app->post('/graphql', [
+        \GraphQL\Upload\UploadMiddleware::class,
+        \GraphQL\Middleware\GraphQLMiddleware::class,
+    ], 'graphql');
+    $app->get('/ping', \Application\Ping\PingHandler::class, 'ping');
+    $app->get('/health', \Application\Health\HealthHandler::class, 'health');
     $app->get('/test-error', function() {
         throw new \RuntimeException('Test error for Whoops');
     }, 'test.error');
