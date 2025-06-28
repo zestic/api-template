aa8dd799d046c493ef593a423c0a607dfda33e90
iampersistent <corvus@zestic.com>
2025-06-28 01:50:54 -0500

    setup oauth endpoints

--- config/routes.php
+++ config/routes.php
@@ -22,4 +22,19 @@ return static function (Application $app, MiddlewareFactory $factory, ContainerI
     $app->get('/test-error', function() {
         throw new \RuntimeException('Test error for Whoops');
     }, 'test.error');
+
+    // OAuth2 Authorization endpoint
+    $app->route('/oauth/authorize', [
+        \Zestic\GraphQL\AuthComponent\Application\Handler\AuthorizationRequestHandler::class,
+    ], ['GET', 'POST'], 'oauth.authorize');
+
+    // OAuth2 Token endpoint
+    $app->post('/oauth/token', [
+        \Zestic\GraphQL\AuthComponent\Application\Handler\TokenRequestHandler::class,
+    ], 'oauth.token');
+
+    // Magic Link Verification endpoint
+    $app->get('/magic-link/verify', [
+        \Zestic\GraphQL\AuthComponent\Application\Handler\MagicLinkVerificationHandler::class,
+    ], 'magic-link.verify');
 };
